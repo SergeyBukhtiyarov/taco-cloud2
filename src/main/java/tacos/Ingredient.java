549b29fccff11d73d00a4ea01240ae70290ce9c9
SergeyBukhtiyarov <bukhtiyarov.s.a@gmail.com>
2022-10-17 15:38:20 +0400

    new taco-cloud -add class Taco -add class TacoOrder -add class Ingredient -add view Home -add view design -add DesignTacoController

---
+++ src/main/java/tacos/Ingredient.java
@@ -0,0 +1,15 @@
+package tacos;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public enum Type{
+        WRAP, PROTEIN, VEGGIES,CHEESE,SAUCE
+    }
+}
---
+++ src/test/java/tacos/TacoCloud2ApplicationTests.java
@@ -0,0 +1,21 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+class TacoCloud2ApplicationTests {
+
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
